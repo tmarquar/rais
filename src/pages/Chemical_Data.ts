faa058a4e3de516dc196009bd4fe666e46a7607c
Talon A. Marquard <6tm@tanzanite.ornl.gov>
2018-07-09 16:01:07 -0400

    Learning to Parse

--- src/pages/Chemical_Data.ts
+++ src/pages/Chemical_Data.ts
@@ -1,14 +1,22 @@
+//import { File } from '@ionic-native/file'
 
 export class ChemicalData {
   _casnum : string;
   _industial_soil = [];
   _industial_tapwater = [];
   _resident_soil = [];
+  _resident = [];
+  _industrial = [];
 
   constructor () {
 
   }
 
+  readRMLcsv():void {
 
+    //var fs = require('fs');
+    //var parse = require('csv-parse');
+
+  }
 
 }
