4c0116dd7d3d2e557007359a2f11f3056456f7f4
Jama M. Charles <6mc@emeraldut.ornl.gov>
2018-07-12 10:22:21 -0400

    still need to but read in data in proper places

--- src/pages/Chemical_Container.ts
+++ src/pages/Chemical_Container.ts
@@ -1,7 +1,6 @@
-
-import * as papa from 'papaparse';
 import { Http } from '@angular/http';
-import {ChemicalData} from './Chemical_Data';
+import { ChemicalData } from './Chemical_Data';
+import * as papa from 'papaparse';
 
 export class ChemicalContainer {
   //_chemicals : ChemicalData[];
@@ -9,23 +8,32 @@ export class ChemicalContainer {
   csvData:any[] = [];
   headerRow:any[] = [];
   _chemicalNames:string[] = [];
+  _chemicalsMasterList: ChemicalData[] = [];
 
-
-  constructor (private http:Http) {
+  constructor (private http: Http) {
     this.readCsvData();
-
   }
 
   initializeChemicalNames() {
-    for (let chemical of this.csvData) {
-
-      this._chemicalNames.push(chemical[0]);
-    }
-
+     for (let chemical of this.csvData) {
+       this._chemicalNames.push(chemical[0]);
+     }
   }
 
   getChemicalNames() : string[] {
-    return this._chemicalNames;
+      return this._chemicalNames;
+  }
+
+  initializeChemicals() {
+    for (let row of this.csvData) {
+      this._chemicalsMasterList[row[0]] = new ChemicalData();
+      this._chemicalsMasterList[row[0]].setChemicalName(row[0]);
+
+      //console.log(this._chemicalsMasterList[row[0]]);
+    //  console.log(chemical[0]);
+    //  console.log(chemical[1]);
+    //  this._chemicalData.push(chemical[1]);
+    }
   }
 
   private readCsvData() {
@@ -37,26 +45,24 @@ export class ChemicalContainer {
   );
 }
 
-private extractData(res) {
-  let csvData = res['_body'] || '';
-  let parsedData = papa.parse(csvData).data;
+  private extractData(res) {
+    let csvData = res['_body'] || '';
+    let parsedData = papa.parse(csvData).data;
 
-  this.headerRow = parsedData[0];
-  parsedData.splice(0,1);
-  this.csvData = parsedData;
-  
-  this.initializeChemicalNames();
-}
+    this.headerRow = parsedData[0];
+    parsedData.splice(0,1);
+    this.csvData = parsedData;
+
+    this.initializeChemicalNames();
+    this.initializeChemicals();
+  }
 
 
-private handleError(err) {
-  console.log('something went wrong: ', err);
-}
-
+  private handleError(err) {
+    console.log('something went wrong: ', err);
+  }
 
   readRMLcsv():void {
-
-
 
   }
 }
