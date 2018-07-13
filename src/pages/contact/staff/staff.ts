8655ef895470300a30c3870d81752ff5c68c98b7
Talon A. Marquard <6tm@tanzanite.ornl.gov>
2018-07-13 11:22:33 -0400

    just in case

--- src/pages/contact/staff/staff.ts
+++ src/pages/contact/staff/staff.ts
@@ -9,10 +9,35 @@ import { NavController, NavParams } from 'ionic-angular';
 export class StaffPage {
   staffMember :string;
   staff : string[];
+  staffEmail : string[];
+  staffTitle : string[];
+  description: string[];
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.staffMember = navParams.get('staffMember');
     this.staff = navParams.get('staff');
+    this.initializeEmail();
+    this.initializeTitle();
+    this.initializeDescription();
+  }
+
+
+  private initializeTitle () : void {
+    staffTitle['Fred Dolislager'] = "Program and Content Manager; Trainer";
+    staffTitle['Leslie Galloway'] = "Web Master, Database Manager, and Tool Developer; Trainer";
+    staffTitle['Debra Stewart'] = "QA/QC; Trainer";
+    staffTitle['Katie Noto'] = "Web/Software Developer";
+    staffTitle['Karessa Manning'] = "Environmental Risk Analyst";
+  }
+
+  private initializeEmail () : void {
+    staffEmail['Fred Dolislager'] = "fdolislager@utk.edu";
+    staffEmail['Leslie Galloway'] = ""
+  }
+
+  private initializeDescription () : void {
+
   }
 
 }
